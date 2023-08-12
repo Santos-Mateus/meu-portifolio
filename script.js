ec63b173cec895e46a60fa47ec25fde19ffe91c4
Santos-Mateus <matteussbt@gmail.com>
2023-08-12 12:32:21 -0300

    Modifica código de animações.

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-function ativaLetra(elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((Letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += Letra;
-        }, 75 * i);
-    });
+
+function escrevendoLetra() {
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((Letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += Letra;
+            }, 75 * i);
+        });
+    }
+    
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
 }
 
+escrevendoLetra();
 
-const titulo = document.querySelector('.digitando');
 
-ativaLetra(titulo);
+const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria');
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x');
+    navMenu.classList.toggle('ativado');
+})
+
+
+
